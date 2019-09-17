0e8458bcb19b4a54b5729783b053145e346d0e53
Guillaume Huysmans <ghuysmans99@gmail.com>
2019-09-17 16:55:35 +0200

    Refined Media.t
    - Removed HD (it's not a media type), added is_hd.
    - Added to_french

--- lib/media.ml
+++ lib/media.ml
@@ -2,7 +2,6 @@ type t =
   | DVD
   | BRD
   | HDDVD
-  | HD
   | UMD
   | BRD_3D
   | UHD
@@ -11,11 +10,14 @@ let is_3d = function
   | BRD_3D -> true
   | _ -> false
 
+let is_hd = function
+  | BRD | HDDVD | BRD_3D | UHD -> true
+  | _ -> false
+
 let to_string = function
   | DVD -> "DVD"
   | BRD -> "BRD"
   | HDDVD -> "HDDVD"
-  | HD -> "HD"
   | UMD -> "UMD"
   | BRD_3D -> "BRD-3D"
   | UHD -> "UHD"
@@ -24,11 +26,18 @@ let of_string = function
   | "DVD" -> DVD
   | "BRD" -> BRD
   | "HDDVD" -> HDDVD
-  | "HD" -> HD
   | "UMD" -> UMD
   | "BRD-3D" -> BRD_3D
   | "UHD" -> UHD
   | s -> failwith @@ "Dvdfr.Media.of_string: " ^ s
+
+let to_french = function
+  | DVD -> "DVD"
+  | BRD -> "Blu-Ray"
+  | HDDVD -> "HD DVD"
+  | UMD -> "UMD"
+  | BRD_3D -> "Blu-ray 3D"
+  | UHD -> "Blu-ray Ultra HD"
 
 let to_xml_light m =
   Xml.Element ("media", [], [Xml.PCData (to_string m)])
